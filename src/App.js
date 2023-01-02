9a5d12c4551bd0006e52507a45622a5f8cef6151
Viktor <bukanovviktor@yandex.ru>
2023-01-02 21:51:25 +0300

    add Partners Block

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './css/App.css';
 import Header from "./components/header";
 import BigInfo from "./components/bigInfo";
+import Partners from "./components/partners";
 
 function App() {
     return(
         <div>
             <Header/>
             <BigInfo/>
+            <Partners/>
         </div>
     );
 }
---
+++ src/components/partners.js
@@ -0,0 +1,69 @@
+function Partners() {
+    return (
+        <div className="partners">
+            <div className="partners_container">
+                <h1 className="partners_container_title">
+                    Card For You – финансовый
+                    <span> помощник.</span>
+                </h1>
+
+                <div className="partners_wrapper">
+                    <p className="partners_text">
+                        Мы собрали сотни предложений от банков и МФО в одном месте, чтобы вы могли сравнить и выбрать
+                        самые лучшие
+                    </p>
+                    <div className="partners_license">
+                        <p>
+                            <img src="/img/partners/gerb.svg" alt=""/>
+                            <span>
+                            Все банки и МФО с лицензией ЦБ
+                        </span>
+                        </p>
+                    </div>
+                    <div className="partners_items">
+                        <p className="partners_title">
+                            Более 100 партнеров
+                        </p>
+                        <div className="partners_items_inner">
+                            <div className="partners_items_items">
+                                <img src="/img/partners/alfa.svg" alt=""/>
+                            </div>
+                            <div className="partners_items_items">
+                                <img src="/img/partners/otkr.svg" alt=""/>
+                            </div>
+                            <div className="partners_items_items">
+                                <img src="/img/partners/sovk.svg" alt=""/>
+                            </div>
+                            <div className="partners_items_items">
+                                <img src="/img/partners/vtb.svg" alt=""/>
+                            </div>
+                            <div className="partners_items_items">
+                                <img src="/img/partners/tin.svg" alt=""/>
+                            </div>
+                            <div className="partners_items_items">
+                                <img src="/img/partners/gaz.svg" alt=""/>
+                            </div>
+                            <div className="partners_items_items">
+                                <img src="/img/partners/sber.svg" alt=""/>
+                            </div>
+                            <div className="partners_items_items">
+                                <img src="/img/partners/money.svg" alt=""/>
+                            </div>
+                            <div className="partners_items_items">
+                                <img src="/img/partners/cesh.svg" alt=""/>
+                            </div>
+                            <div className="partners_items_items">
+                                <img src="/img/partners/credit.svg" alt=""/>
+                            </div>
+                            <div className="partners_items_items">
+                                <img src="/img/partners/ekso.svg" alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Partners;
